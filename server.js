53c6b2a610b6846d9cca634b428cb8a879413e00
khaolakkkkk <khaola.najar@gmail.com>
2023-11-15 11:52:46 +0100

    creation api

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+require('dotenv').config();
+let cors = require('cors');
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    database:process.env.DB_DATA ,
+    user:process.env.DB_USER ,
+    password:process.env.DB_PWD ,
+});
+app.use(cors());
+ 
+app.get('/api/articles/', async (req, res) => {
+    console.log("lancement de la connexion");
+    const conn = await pool.getConnection();
+    console.log("lancement de la requete");
+    const rows = await conn.query("SELECT * FROM articles");
+    console.log(rows);
+    res.status(200).json(rows);
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log("Serveur a l'ecoute");
+});
+ 
